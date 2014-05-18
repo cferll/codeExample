ea5cfd01313005e58113c7a32050d1657728f496
camino <cferll@unileon.es>
2014-05-18 18:10:46 +0200

    Introducido comentario del main

--- src/main/java/es/unileon/schoolSpring/example01/TechShop.java
+++ src/main/java/es/unileon/schoolSpring/example01/TechShop.java
@@ -3,6 +3,14 @@ package es.unileon.schoolSpring.example01;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * Main class for the introductory example to Spring shown
+ * in the video "Introduccion a Spring Framework" de
+ * SpringHispano.org
+ * 
+ * @author camino
+ *
+ */
 public class TechShop {
 	public static void main(String[] args){
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("./context.xml");
